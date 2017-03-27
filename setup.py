1087c5fe31f19e09e8f47d27498e70d00951f378
Aubrey Stark-Toller <aubrey@kleetope.net>
2017-03-28 00:22:22 +0100

    import

---
+++ setup.py
@@ -0,0 +1,39 @@
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(
+    name='api-mimic',
+    version='0.1.0',
+    description='API mimicry',
+    author='Aubrey Stark-Toller',
+    author_email='aubrey@kleetope.uk',
+    license='BSD',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL3 License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=['api_mimic'],
+    tests_require = ['pytest', 'pytest-cov',],
+    cmdclass = {'test': PyTest},
+)
---
+++ tests/test_object_proxy.py
@@ -0,0 +1,58 @@
+import importlib
+import pytest
+from unittest import mock
+from contextlib import contextmanager
+
+
+def api():
+    def funcA(a, b, c):
+        pass
+
+    def funcB(a='x', b=2, c=None):
+        pass
+
+    def funcC(a, b, c, d=None, *args, **kwargs):
+        pass
+
+    return {
+        'funcA': funcA,
+        'funcB': funcB,
+        'funcC': funcC,
+    }
+
+
+@contextmanager
+def mock_callback(*args, **kwargs):
+    callback = mock.Mock()
+    yield callback
+    callback.assert_called_once_with(*args, *kwargs)
+
+
+@pytest.fixture()
+def full_mimic():
+    module = importlib.import_module('api_mimic')
+    return module.full_mimic_factory(api())
+
+
+@pytest.fixture()
+def shallow_mimic():
+    module = importlib.import_module('api_mimic')
+    return module.shallow_mimic_factory(api())
+
+
+@pytest.mark.parametrize("test_args", [
+    ("a", "b", "c"),
+    (1, 2, 3),
+])
+def test_full_mimic_positonal_only(test_args, full_mimic):
+    with mock_callback('funcA', test_args, {}) as callback:
+        full_mimic(callback).funcA(*test_args)
+
+@pytest.mark.parametrize("test_args,expected_kwargs", [
+    ({"a": True, "b": "t", "c": 2}, (True,"t",2)),
+    ({"a": 5, "b": None}, (5,None,None)),
+    ({"b": False}, ("x",False,None)),
+])
+def test_full_mimic_only_maybe_kwargs_as_only_kwargs(test_args, expected_kwargs, full_mimic):
+    with mock_callback('funcB', expected_kwargs, {}) as callback:
+        full_mimic(callback).funcB(**test_args)
