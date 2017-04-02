60b6252dfdd19e88346ab41e9a45389922bbc45e
Aubrey Stark-Toller <aubrey@kleetope.net>
2017-04-02 17:22:47 +0100

    Clean up
    Got rid of converters
    Tightened coverage reporting
    Made tests more modular - possibly now pointless?
    Updated README to reflect changes

--- api_mimic/__init__.py
+++ api_mimic/__init__.py
@@ -1,5 +1,70 @@
-from .converters import module_to_api
-from .factories import full_mimic_factory, shallow_mimic_factory
+from . import compat
 
 
-__all__ = ['full_mimic_factory', 'shallow_mimic_factory', 'module_to_api']
+def iface_init(self, callback):
+    self.callback = callback
+
+
+def mimic_factory(target):
+    attrs = {'__init__': iface_init}
+
+    for op_func_name, op_func in target.items():
+        op_func_sig = compat.signature(op_func)
+
+        arg_names = []
+        kwarg_names = []
+        unbound_args = None
+        unbound_kwargs = None
+        kwargs_seen = False
+        proxy_op_func_str_args = "self"
+
+        for param in op_func_sig.parameters.values():
+            if (
+                    param.kind == compat.Parameter.POSITIONAL_ONLY or
+                    param.kind == compat.Parameter.POSITIONAL_OR_KEYWORD
+            ):
+                arg_names.append(param.name)
+            elif param.kind == compat.Parameter.VAR_POSITIONAL:
+                unbound_args = param.name
+            elif param.kind == compat.Parameter.VAR_KEYWORD:
+                unbound_kwargs = param.name
+            else: 
+                # param.kind == compat.Parameter.KEYWORD_ONLY
+                # this is last otherwise this chokes with Python < 3
+                if not unbound_args and not kwargs_seen:
+                    proxy_op_func_str_args += ',*'
+                kwargs_seen = True
+                kwarg_names.append(param.name)
+
+            proxy_op_func_str_args += ',' + str(param)
+
+        func_body = []
+
+        func_body.append('saved_args = [' + ', '.join(arg_names) + ']')
+        if unbound_args is not None:
+            func_body.append('saved_args.extend(' + unbound_args + ')')
+
+        func_body.append(
+            'saved_kwargs = {{{0}}}'
+            .format(', '.join('"{0}": {0}'.format(n) for n in kwarg_names))
+        )
+        if unbound_kwargs is not None:
+            func_body.append('saved_kwargs.update({})'.format(unbound_kwargs))
+
+        func_body.append(
+            'return self.callback("{}", tuple(saved_args), saved_kwargs)'
+            .format(op_func_name)
+        )
+
+        code = (
+            "def {}({}):\n    {}\n"
+            .format(
+                op_func_name,
+                proxy_op_func_str_args,
+                '\n    '.join(func_body)
+            )
+        )
+
+        exec(code, None, attrs)
+
+    return type('Proxy', (object,), attrs)
--- tests/test_factories.py
+++ tests/test_factories.py
@@ -1,173 +1,6 @@
-import itertools
-import sys
-import importlib
 import pytest
-import random
-try:
-    from collections import UserDict
-except ImportError:
-    from UserDict import IterableUserDict as UserDict
-try:
-    from unittest import mock
-except ImportError:
-    import mock
-from contextlib import contextmanager
-
-NAMES = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-DEFAULTS = ('"x"', True, None, 2, '"22"', '"aa"', (1,2,3,'"a"'), None, 2, 45, False, '"x"', '"2"', '"5a"')
-
-
-class API(UserDict):
-    def __str__(self):
-        s = []
-        for name, func in self.items():
-            s.append(name + ':' + func.code)
-        return '{' + ', '.join(s) + '}'
-
-    def __repr__(self):
-        s = []
-        for name, func in self.items():
-            s.append('{}: ({}, {}, {}, {}, {}, {})[{}]'.format(
-                name,
-                func.sig.args,  func.sig.args_with_default, func.sig.unbound_args,
-                func.sig.kwargs,  func.sig.kwargs_with_default, func.sig.unbound_kwargs,
-                func.code.strip()
-            ))
-        return '{' + ', '.join(s) + '}'
- 
- 
-
-class Sig:
-    def __init__(self, args, args_with_default, unbound_args, kwargs, kwargs_with_default, unbound_kwargs):
-        self.args = args
-        self.args_with_default = args_with_default
-        self.unbound_args = unbound_args
-        self.kwargs = kwargs
-        self.kwargs_with_default = kwargs_with_default
-        self.unbound_kwargs = unbound_kwargs
-        self._str = None
-
-    def arg_count(self):
-        return self.args + self.args_with_default
-
-    def kwarg_count(self):
-        return self.kwargs + self.kwargs_with_default
-
-    def all_args_count(self):
-        return self.kwargs + self.kwargs_with_default + self.args + self.args_with_default
-
-    def __str__(self):
-        if self._str is None:
-            seed = []
-            args = []
-            kwargs = []
-            names = iter(NAMES)
-            defaults = iter(DEFAULTS)
-
-            for _ in range(self.args):
-                label = next(names)
-                args.append(label)
-                seed.append(label)
-
-            for _ in range(self.args_with_default):
-                arg = next(names) + '=' + str(next(defaults))
-                args.append(arg)
-                seed.append(arg)
-
-            if self.unbound_args:
-                seed.append('*args')
-
-            for _ in range(self.kwargs):
-                label = next(names)
-                kwargs.append(label)
-                seed.append(label)
-
-            for _ in range(self.kwargs_with_default):
-                arg =  next(names) + '=' + str(next(defaults))
-                kwargs.append(arg)
-                seed.append(arg)
-
-            if self.unbound_kwargs:
-                seed.append('**kwargs')
-
-            if kwargs:
-                seed = ','.join(seed)
-                random.seed(seed)
-                random.shuffle(kwargs)
-
-            sig = args
-
-            if self.unbound_args:
-                sig.append('*args')
-
-            if kwargs:
-                if not self.unbound_args:
-                    sig.append('*')
-                sig.extend(kwargs)
-
-            if self.unbound_kwargs:
-                sig.append('**kwargs')
-
-            self._str = ",".join(sig)
-
-        return self._str
-
-    def __repr__(self):
-        return 'Sig({}, {}, {}, {}, {}, {})'.format(
-            self.args,
-            self.args_with_default,
-            self.unbound_args,
-            self.kwargs,
-            self.kwargs_with_default,
-            self.unbound_kwargs
-        )
-
-
-def make_api(*sigs):
-    final = {}
-    attrs = API()
-    for idx, sig in enumerate(sigs):
-        func_name = "func" + str(idx)
-        code = 'def {0}({1}): pass\n'.format(func_name, sig)
-        try:
-            exec(code, final, attrs)
-        except SyntaxError:
-            raise Exception(sig)
-        attrs[func_name].sig = sig
-        attrs[func_name].code = code
-    return attrs
-
-
-def null_func(*args, **kwargs):
-    pass
-
-
-def _sig_permutations():
-    params = itertools.product(range(3), range(3), (False, True), range(3), range(3), (False, True))
-
-    for param in params:
-        res = Sig(*param)
-        if sys.version_info < (3,3) and (param[3] != 0 or param[4] != 0):
-            res = pytest.mark.skip(res, reason="keyword only arguments don't exist before Python 3")
-
-        yield res
-
-
-@pytest.fixture(scope="module", params=list(_sig_permutations()))
-def singleton_api(request):
-    return make_api(request.param)
-
-
-@pytest.fixture()
-def full_mimic():
-    module = importlib.import_module('api_mimic')
-    return module.full_mimic_factory
-
-
-@pytest.fixture()
-def shallow_mimic():
-    module = importlib.import_module('api_mimic')
-    return module.shallow_mimic_factory
+import itertools
+from pytest import local as l
 
 
 @pytest.mark.parametrize("test_args,test_kwargs", list(itertools.product(
@@ -185,12 +18,12 @@ def shallow_mimic():
         {"a":"g", "b":None, "c":"6f", "d":5, "e": 5, "f":"q"},
     )))
 )
-def test_my_test(full_mimic, singleton_api, test_args, test_kwargs):
+def test_my_test(mimic, singleton_api, test_args, test_kwargs):
     func_name = next(iter(singleton_api))
     sig = singleton_api[func_name].sig
-    cls = full_mimic(singleton_api)
+    cls = mimic(singleton_api)
 
-    extra = sum(1 for k in test_kwargs.keys() if NAMES.index(k) < sig.args)
+    extra = sum(1 for k in test_kwargs.keys() if l.NAMES.index(k) < sig.args)
 
     if (
         # to few args
@@ -200,14 +33,14 @@ def test_my_test(full_mimic, singleton_api, test_args, test_kwargs):
         # to many kwargs
         (not sig.unbound_kwargs and (sig.all_args_count()) < len(test_kwargs)) or
         # arg provided using both parg and kwarg
-        (test_kwargs and any(NAMES.index(k) < min(sig.arg_count(), len(test_args)) for k in test_kwargs.keys())) or
+        (test_kwargs and any(l.NAMES.index(k) < min(sig.arg_count(), len(test_args)) for k in test_kwargs.keys())) or
         # values provided for non-existant kwargs
-        (not sig.unbound_kwargs and any(NAMES.index(k) >= sig.all_args_count() for k in test_kwargs.keys())) or
+        (not sig.unbound_kwargs and any(l.NAMES.index(k) >= sig.all_args_count() for k in test_kwargs.keys())) or
         # required kwargs are missing
-        (sig.kwargs != 0 and len(set(NAMES[sig.arg_count():sig.arg_count() + sig.kwargs]).difference(test_kwargs.keys())) > 0)
+        (sig.kwargs != 0 and len(set(l.NAMES[sig.arg_count():sig.arg_count() + sig.kwargs]).difference(test_kwargs.keys())) > 0)
     ):
         with pytest.raises(TypeError):
-            getattr(cls(null_func), func_name)(*test_args, **test_kwargs)
+            getattr(cls(l.null_func), func_name)(*test_args, **test_kwargs)
 
     else:
         expected_args = test_args
@@ -215,7 +48,7 @@ def test_my_test(full_mimic, singleton_api, test_args, test_kwargs):
         additional = []
 
         for name, value in test_kwargs.items():
-            idx = NAMES.index(name)
+            idx = l.NAMES.index(name)
             if idx <= sig.arg_count():
                 additional.append((idx, value))
             else:
@@ -223,7 +56,7 @@ def test_my_test(full_mimic, singleton_api, test_args, test_kwargs):
 
         expected_args += tuple(x[1] for x in sorted(additional, key=lambda t: t[0]))
 
-        callback = mock.Mock()
+        callback = l.mock.Mock()
         getattr(cls(callback), func_name)(*test_args, **test_kwargs)
 
         callback.called_once_with(func_name, expected_args, expected_kwargs)
